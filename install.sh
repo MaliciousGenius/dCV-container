5a7698f109240e1f0b4eaa6ed32a33d5edf32daf
Dmitriy <maliciousgenius@gmail.com>
2017-08-15 12:58:44 +0300

    Рефакторинг и исправления

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
-chmod +x /tmp/docker-compose
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
-docker-compose -f ./docker/Dockerfile build
-docker-compose -f ./docker/Dockerfile up
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose up
