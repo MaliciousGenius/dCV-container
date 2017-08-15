762aedccc262261e61fd975444e3bf4b99ecc561
Dmitriy <maliciousgenius@gmail.com>
2017-08-15 13:32:27 +0300

    fix

--- install.sh
+++ install.sh
@@ -4,15 +4,14 @@ sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
+    software-properties-common \
     curl
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
+sudo apt-cache policy docer-ce
 sudo apt-get install -y docker-ce
 
 curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
